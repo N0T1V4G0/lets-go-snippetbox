8c851fb91c73cccc2f89a63432e64bf767fe3c11
Lucas Barbosa <lukeb.trybe@gmail.com>
2023-04-01 17:32:05 -0300

    feat: add snippetView & snippetCreate handlers

--- main.go
+++ main.go
@@ -10,9 +10,19 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Let's Go!"))
 }
 
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Showing a snippet..."))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Creating a snippet..."))
+}
+
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	fmt.Println("Starting app on port 3000")
 	// ListenAndServe always return non-nil err
