597c724fec4ad620c4d2eca468eb112b8b91c4d7
Lucas Barbosa <lukeb.trybe@gmail.com>
2023-04-01 17:24:00 -0300

    feat: add web server

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Let's Go!"))
+}
 
 func main() {
-	fmt.Println("Let's Go!")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+
+	fmt.Println("Starting app on port 3000")
+	// ListenAndServe always return non-nil err
+	err := http.ListenAndServe(":3000", mux)
+	log.Fatal(err)
 }
