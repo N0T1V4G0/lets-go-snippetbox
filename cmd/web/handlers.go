5189bb4d16dce52d10c215d0f1204f8616ab2cfe
Lucas Barbosa <lukeb.trybe@gmail.com>
2023-05-04 13:22:02 -0300

    refactor: render snippets in home tmpl

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -16,14 +16,10 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// snippets, err := app.snippets.Latest()
-	// if err != nil {
-	// 	app.serverError(w, err)
-	// }
-
-	// for _, snippet := range snippets {
-	// 	fmt.Fprintf(w, "%+v\n\n", snippet)
-	// }
+	snippets, err := app.snippets.Latest()
+	if err != nil {
+		app.serverError(w, err)
+	}
 
 	files := []string{
 		// "base" template must be placed first
@@ -38,7 +34,11 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	err = ts.ExecuteTemplate(w, "base", nil)
+	data := &templateData{
+		Snippets: snippets,
+	}
+
+	err = ts.ExecuteTemplate(w, "base", data)
 	if err != nil {
 		app.serverError(w, err)
 	}
--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -3,5 +3,6 @@ package main
 import "github.com/N0T1V4G0/lets-go-snippetbox/internal/models"
 
 type templateData struct {
-	Snippet *models.Snippet
+	Snippet  *models.Snippet
+	Snippets []*models.Snippet
 }
